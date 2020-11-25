6d404e62c4e40fc24bacdd24ccf0b7137f4db1c6
Anthony <azelinsky1996@gmail.com>
2020-11-24 20:04:07 -0500

    Restructuring code

--- pricing_service/models/store.py
+++ pricing_service/models/store.py
@@ -1,7 +1,6 @@
 import re
 
 from uuid import uuid4
-
 from pricing_service.models.model import Model
 
 
@@ -26,15 +25,15 @@ class Store(Model):
         }
     
     @classmethod
-    def get_by_name(cls, store_name: str)-> cls:
+    def get_by_name(cls, store_name: str)-> "Store":
         return cls.find_by_one(attribute="name", value=store_name)
     
     @classmethod
-    def get_by_url_prefix(cls, url_prefix: str)-> cls:
+    def get_by_url_prefix(cls, url_prefix: str)-> "Store":
         url_regex = {"$regex": f"^{url_prefix}"}
         return cls.find_by_one(attibute="url_prefix", value=url_regex)
 
     @classmethod
-    def find_by_url(cls, url: str)-> cls:
+    def find_by_url(cls, url: str)-> "Store":
         pattern = re.compile(r"https?://.*?/")
         return cls.get_by_url_prefix(pattern.search(url).group(1))
--- pricing_service/views/alerts.py
+++ pricing_service/views/alerts.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request
-from pricing_service.models.alert import Alert 
+from pricing_service.models.alert import Alert
+from pricing_service.models.store import Store
+from pricing_service.models.item import Item
 
 
 alert_blueprint = Blueprint("alerts", __name__)
@@ -14,9 +16,11 @@ def index():
 @alert_blueprint.route("/new", methods=["GET", "POST"])
 def new_alert():
     if request.method == "POST":
-        item_id = request.form['item_id']
+        item_url = request.form['item_url']
         price_limit = request.form['price_limit']
+        store  = Store.find_by_url(item_url)
+        item = Item(item_url, store.tag_name, store.query)
 
-        Alert(item_id, price_limit).save_to_mongo()
+        Alert(item._id, price_limit).save_to_mongo()
 
     return render_template("alerts/new_alert.html")
